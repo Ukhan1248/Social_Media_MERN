add907e1d583d88b6968e55fa9cb0fab45aae211
Umair Khan <umairkhan814@icloud.com>
2022-04-12 14:27:19 -0500

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import memories from "./images/memories.png";
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxwidth="lg">
+      <AppBar className={classes.AppBar} position="static" color="inherit">
+        <Typography className={classes.heading} varient="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import useStyles from "./styles";
+const Form = () => {
+  const classes = useStyles();
+  return <h1>FORM</h1>;
+};
+
+export default Form;
