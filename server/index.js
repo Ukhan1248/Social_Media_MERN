f72b3ed56e2ab2a921891edb713cd672f5f70470
Umair Khan <umairkhan814@icloud.com>
2022-04-25 16:51:21 -0500

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("APP IS RUNNING.");
+});
+
+const CONNECTION_URL =
+  "mongodb+srv://umairkmernsite123:umairkmernsite123@cluster0.nlnab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
+  )
+  .catch(error => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
