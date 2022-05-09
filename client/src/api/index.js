86ea671c133421b6c7dae8cf443ab68d36e4a8fd
Umair Khan <umairkhan814@icloud.com>
2022-05-09 11:45:51 -0500

    Add files via upload

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,34 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/posts";
+const API = axios.create({ baseURL: "https://mernuk.herokuapp.com/" });
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = newPost => axios.post(url, newPost);
+API.interceptors.request.use(req => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
+
+export const fetchPost = id => API.get(`/posts/${id}`);
+export const fetchPosts = page => API.get(`/posts?page=${page}`);
+export const fetchPostsByCreator = name =>
+  API.get(`/posts/creator?name=${name}`);
+export const fetchPostsBySearch = searchQuery =>
+  API.get(
+    `/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${
+      searchQuery.tags
+    }`
+  );
+export const createPost = newPost => API.post("/posts", newPost);
+export const likePost = id => API.patch(`/posts/${id}/likePost`);
+export const comment = (value, id) =>
+  API.post(`/posts/${id}/commentPost`, { value });
 export const updatePost = (id, updatedPost) =>
-  axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = id => axios.delete(`${url}/${id}`);
-export const likePost = id => axios.patch(`${url}/${id}/likePost`);
+  API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = id => API.delete(`/posts/${id}`);
+
+export const signIn = formData => API.post("/user/signin", formData);
+export const signUp = formData => API.post("/user/signup", formData);
