741405cef29503fb85d6cc459f57cae7a4eb4f64
Umair Khan <umairkhan814@icloud.com>
2022-05-13 14:11:12 -0500

    Add files via upload

---
+++ client/src/actions/auth.js
@@ -0,0 +1,28 @@
+import { AUTH } from "../constants/actionTypes";
+import * as api from "../api/index.js";
+
+export const signin = (formData, history) => async dispatch => {
+  try {
+    const { data } = await api.signIn(formData);
+    // log in the user
+
+    dispatch({ type: AUTH, data });
+
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, history) => async dispatch => {
+  try {
+    // sign up the user
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
